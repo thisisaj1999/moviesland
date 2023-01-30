11435fafec8adc7169b2966f5ddfd99797d78a9a
thisisaj1999 <atuljoshi94170@gmail.com>
2023-01-30 18:16:19 +0530

    dotenv added.

--- src/App.js
+++ src/App.js
@@ -5,16 +5,14 @@ import './App.css';
 import MovieCard from './MovieCard';
 import searchIcon from './search.svg';
 
-// 271c08aa
-
-const API_URL = 'http://www.omdbapi.com?apikey=271c08aa';
+dotenv.config();
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
 
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
+    const response = await fetch(`${process.env.API_URL}&s=${title}`);
     const data = await response.json();
 
     setMovies(data.Search);
